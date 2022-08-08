c35f6d7b3588f2c80fbc67a57b72b4c8e7edf624
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-08-08 16:36:36 +0000

    C2 seems to work but using array method (not fast enough); now will move to SegTree

---
+++ Facebook/2020/Round2/B.cpp
@@ -0,0 +1,34 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+// double dp[8001][8001]; // dp[n][a] Pr(a is still here after n matches);
+
+// int main() {
+  
+//   //May remove in case of interactive problem or need to use c streams
+//   ios_base::sync_with_stdio(false);
+//   cin.tie(NULL);
+  
+//   int T;
+//   int ans;
+  
+//   scanf("%d",&T);
+//   for (int t = 1;t<=T;t++) {
+//     int N;
+//     double pBetterWins;
+//     scanf("%d",&N);
+//     scanf("%lf",&pBetterWins);
+
+//     memset(dp[0],1,sizeof(dp[0])); // after 0 matches, all will be there.
+
+//     for (int a=0;a<N;++a) {
+//       for (int n=0;n<N-1;++n) {
+//         dp[n][]
+//       }
+//     }
+    
+
+//     printf("Case #%d: %d\n",t,ans);
+//   }
+// }
--- Facebook/2021/Round2/C2.cpp
+++ Facebook/2021/Round2/C2.cpp
@@ -1,24 +1,13 @@
-//NOT DONE Yet.
-
-//O(RC)
-
-//A removal of a single car can only affect one square and thus can only give you a maximum benefit of one square in the end.
-//Therefore it is never more beneficial to remove a car then shift the empty square into the current row than to shift the rows first and then remove the car.
-//This means that we can do all of the shifting first and then the removal of individual cars.
-//It can also be seen that there is nobenefit to shifting down and then up or vice versa. While this isn't the same as doing nothing, it cannot have better result than just an up shift for example.
-//As a result, we remove the need for branching from the solution because we can try all up shifts first, then subsequently individual removals. Then we try all down shifts and all subsequent individual removals.
-//The brute force solution would be at least O(3^C) as you have to try up to C moves to clear the row, and there are 3 moves per possibility. This is ridiculously large with R = 2M.
-//This way means that we only have to try O(C) moves.
-//However we still need to ensure that we can perform each move in O(C) or O(R) or better to avoid TLE. This is trivial for the remove one car moves, but for the shift moves we need a mechanism which doesn't involve actually performing the shifts. We can do this by keeping track of the number of free spaces above and below the current row, and using this to see if gridlock has occurred:
-    //Find amount of free spaces above row K in each column
-    //Each time you push an X up the number of free spaces above reduces. 
-    //IF the number of free spaces above is 0 we are in gridlock; no further changes
-    //If the number of free spaces above is more than 0, then we get whatever is in the row (K+shift) which is located below, since gridlock is the only thing which prevents this, i.e. without gridlock everything would effectively shift.
-    //Repeat for downward shifts.
-    
-
 #include <bits/stdc++.h>
+#include <bits/extc++.h>
+
+using namespace __gnu_pbds;
 using namespace std;
+
+// Order Statistics Tree
+typedef tree<int, null_type, less<int>, rb_tree_tag,
+             tree_order_statistics_node_update>
+    OST;
 
 int main() {
   int T; scanf("%d",&T);
@@ -27,11 +16,6 @@ int main() {
     vector<vector<bool>> garage(R,vector<bool>(C));   //Is there a car here?
     vector<int> spacesAbove(C,0);
     vector<int> spacesBelow(C,0);
-    vector<int> shifts;
-
-    vector<vector<int>> freeAboveBecomes(C,vector<int>(R,-1));
-    vector<vector<int>> freeBelowBecomes(C,vector<int>(R,-1));
-    
     
     for (int r=0;r<R;++r) {
       string str;
@@ -46,117 +30,162 @@ int main() {
         }        
       }
     }
-    for (int c=0;c<C;++c) {
-      freeBelowBecomes[c][spacesBelow] = 0;
-      freeAboveBecomes[c][spacesBelow] = 0; 
-    }
-
 
     vector<pair<int,int>> spells;
     for (int s=0;s<S;++s) {
       int r,c;
       scanf("%d %d",&r,&c);
+      r--; c--;
       spells.emplace_back(r,c);
     }
     
-
     vector<bool> rowK = garage[K];
-    int best,total = 0;
+    int total = 0;
+    vector<int> shiftCosts(R+2); // Will become a segment tree.
+    vector<OST> cars(C);
 
-    //For all downward shifts:
-     for (int shift=1;shift<=K+1;shift++) {
+    //Downward shifts    
+    for (int shift=1;shift<=K+1;shift++) {
       total = 0;
       for (int c=0;c<C;++c) {
         if (spacesBelow[c] > 0 || rowK[c] == false) { //Can we push into this space?
           //...Only if no gridlock or gridlock below and this space is empty.
           
           if (rowK[c]) --spacesBelow[c]; //pushed down a car
-          freeBelowBecomes[c][spacesBelow+1] = shift;
           if (K-shift < 0) 
             rowK[c] = 0;
           else
             rowK[c] = garage[K-shift][c];
         }
         total += rowK[c];
+        
+        if (K-shift >= 0 && garage[K-shift][c]) {
+          cars[c].insert(K+1-shift);
+        }
+        
       }
-      shifts.push_back(shift+total);
+      shiftCosts[K+1-shift] = shift+total;
     }
 
+    rowK = garage[K];
+    total = 0;
     //No shifts, total Xs.
     for (int c=0;c<C;++c) {
       total += rowK[c];
-    }
-    shifts.push_back(total);
+      if (garage[K][c]) {
+        cars[c].insert(K+1);
+      }
+    }    
+    shiftCosts[K+1] = (total);
     
-
-    //Repeat for upward shifts
+    //For all upward shifts:
     rowK = garage[K];
-
-    for (int shift=1;shift<=R-K+1;shift++) {
+    for (int shift=1;shift<=R-K;shift++) {
       total = 0;
       for (int c=0;c<C;++c) {
         if (spacesAbove[c] != 0 || rowK[c] == false) { //Can we push into this space? (NOTE: TCs a little weak; don't require rowK[c] == false here.)
           //...  Only if no gridlock or gridlock above and this space is empty. 
           
           if (rowK[c]) --spacesAbove[c]; //pushed up a car so one fewer empty space. 
-          freeAboveBecomes[c][spacesAbove+1] = shift;
           if (K+shift >= R) //Create new empty spaces from the bottom
             rowK[c] = 0;
           else
             rowK[c] = garage[K+shift][c];
         }
         total += rowK[c]; //Total number of one car at a time moves needed.
+        if (K+shift < R && garage[K+shift][c]) {
+          cars[c].insert(K+1+shift);
+        }
       }
-      shifts.push_back(total+shift);
+      shiftCosts[K+1+shift] = (shift+total);
     }
 
-
+    int M_sum = 0;
+    int prevGridlock, currGridlock, prevGridlock2;
+    
     for (auto [r,c] : spells) {
       int shift = r+1;
+      
       if (garage[r][c] == true) { //Wong is removing a car
-        if (shift >= K+1) {
-          
-        }
-        else if (shift < K+1) { //upward shift to access
-          int shiftUpValue = shift-(K+1);
-
-if fi
-
-          
-          if (freeAboveBecomes[c][0] < shiftUpValue) //Filling gridlock occurs with a lesser shift than this one, so no change.
-          else if (freeAboveBecomes[c][0] > shiftUpValue) { //Filling gridlock occurs currently at a greater shift than this one.
-            if (freeAboveBecomes[c][1] < shiftUpValue ) { //Non-filling gridlock occurs currently at a lesser shift than this one
-                //Filling gridlock occurs at this one
-            }
-            else if (freeAboveBecomes[c][1] > shiftUpValue) { //Non-filling gridlock occurs currently at a greater shift than this one
-              //filling gridlock occurs at the point non-filling gridlock previously occurred
-            }
-
-            if it never occurs ????
-          }
-          else {
-            cout << "should not occur" << endl;
-          }
-            
-  
-          
-        }
-
+        shiftCosts[shift] -= 1;  
         
-        shifts[shift] += 1;
-        garage[r][c] = false;
-      }
-      else {
-        
-        shifts[shift] -= 1;
-        garage[r][c] = true;
-      }
+        if (cars[c].find_by_order(K) != cars[c].end()) { //gridlock occurs one later now than it did before
+          prevGridlock = *cars[c].find_by_order(K); //shift at which gridlock currently occurs or end...
+        }
+        else {
+          prevGridlock = currGridlock = shiftCosts.size();
+        }
+        if (cars[c].find_by_order(cars[c].size()-(R-K)) == cars[c].end()) { //doesn't occur now.
+          prevGridlock2 = -1;
+        }
+        else {
+          prevGridlock2 = *cars[c].find_by_order(cars[c].size()-(R-K));
+        }
+        cars[c].erase(shift);
+        if (cars[c].find_by_order(K) == cars[c].end()) { //doesn't occur now.
+          currGridlock = shiftCosts.size();
+        }
+        else {currGridlock = *cars[c].find_by_order(K);}
 
-        or K minus shift.
+        for (int g=prevGridlock+1;g<currGridlock;++g) {
+          shiftCosts[g] -= 1;
+        }
+        
+        if (cars[c].find_by_order(cars[c].size()-(R-K)) == cars[c].end()) { //doesn't occur now.
+          currGridlock = -1;
+        }
+        else {currGridlock = *cars[c].find_by_order(cars[c].size()-(R-K));}
+
+        for (int g=currGridlock+1;g<prevGridlock2;++g) {
+          shiftCosts[g] -= 1;
+        }
+        
+      }
+      else { //Wong is adding a car
+        shiftCosts[shift] += 1;  
+        if (cars[c].find_by_order(K) == cars[c].end()) { //gridlock occurs one later now than it did before
+          prevGridlock = shiftCosts.size();
+        }
+        else
+          prevGridlock = *cars[c].find_by_order(K); //shift at which gridlock currently occurs or end...
+
+        if (cars[c].find_by_order(cars[c].size()-(R-K)) == cars[c].end()) { //gridlock occurs one later now than it did before
+          prevGridlock2 = -1;
+        }
+        else
+          prevGridlock2 = *cars[c].find_by_order(cars[c].size()-(R-K)); //shift at which gridlock currently occurs or end...
+        
+        
+        cars[c].insert(shift);
+        
+        if (cars[c].find_by_order(K) == cars[c].end()) { //gridlock occurs one later now than it did before
+          currGridlock = shiftCosts.size();
+        }
+        else
+          currGridlock = *cars[c].find_by_order(K); //shift at which gridlock currently occurs or end...
+
+        for (int g=currGridlock+1;g<prevGridlock;++g) {
+          shiftCosts[g] += 1;
+        }
+
+        if (cars[c].find_by_order(cars[c].size()-(R-K)) == cars[c].end()) { //gridlock occurs one later now than it did before
+          currGridlock = -1;
+        }
+        else
+          currGridlock = *cars[c].find_by_order(cars[c].size()-(R-K)); //shift at which gridlock currently occurs or end...
+
+        for (int g=prevGridlock2+1;g<currGridlock;++g) {
+          shiftCosts[g] += 1;
+        }
+      
+      }
+      garage[r][c] = !garage[r][c];
+
+      M_sum += *min_element(shiftCosts.begin(),shiftCosts.end());
+      
     }
-
-
-    printf("Case #%d: %d\n",t,best);
+    
+    printf("Case #%d: %d\n",t,M_sum);
   }
   
   return 0;
