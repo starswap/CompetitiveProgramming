ce71ec3f48e01d605c5a672ebac6b715fd6f0068
Hamish Starling <hamishstarling@hotmail.co.uk>
2022-09-22 15:09:02 +0100

    Add thoughts to A.cpp after reading answers

--- Facebook/2019/Round2/A.cpp
+++ Facebook/2019/Round2/A.cpp
@@ -7,7 +7,7 @@ bool canForceHimToSurrender() {
     if (N == 1 || M == 1) return true;
     else if (K == 1) return false;
     else {
-        int delta1 = abs(A-R1) + abs(B-C1);
+        int delta1 = abs(A-R1) + abs(B-C1); //Here we say they can force him to surrender iff the number of squares from Mr X to each agent is even - which is the same as saying they are on the same colour chess squares. The answers say Mr X is caught iff the even/oddness of the total distance from the top left square to Mr X. and each of the two agents is the same, which is clearly the same thing as saying they have to be on the same chessboard square colours, and thus the same as my method.
         int delta2 = abs(A-R2) + abs(B-C2);
         return (delta1 % 2 == 0) && (delta2 % 2 == 0);
     }
