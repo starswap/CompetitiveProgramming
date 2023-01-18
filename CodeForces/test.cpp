9250160a904371fcb1deb7005ed535edde6f3d9a
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-01-18 19:36:16 +0000

    Add a couple of templates

---
+++ CodeForces/test.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    
+    for (int t=0; t<T; t++) {
+        int N; cin >> N;
+        
+    }
+        
+}
