87e3270c81bd2252c7bfc0239904257a43674cb6
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-02-08 13:50:25 +0000

    Update team notebook

--- ICPC/teamNotebook.cpp
+++ ICPC/teamNotebook.cpp
@@ -102,17 +102,42 @@ bool pointInPolygon(point p, polygon poly) {
 
 
 /* DP */
+// LIS
+vector<int> p;
+int endPoint;
+vector<int> nums;
 
-vector<int> lis(const vector<int>& xs) {
-    vector<int> res;
-    for (int x : xs)
-        if (res.size() == 0 || x > *res.rbegin())
-            res.push_back(x);
-        else
-            res[lower_bound(res.begin(), res.end(), x) - res.begin()] = x;
-            // (in Python: idx = bisect.bisect_left(res, num))
-    return res;
+void printLis(int endPointL) {
+  if (endPointL == -1) return;
+  printLis(p[endPointL]);
+  cout << nums[endPointL];
+  if (endPoint == endPointL)
+    cout << endl;
+  else
+    cout << " ";
 }
+
+
+int k = 0;
+p.assign(n,0);
+vector<int> endValLengthL(n,0);
+vector<int> endILengthL(n,0);
+
+for (int i=0;i<n;++i) {
+    int ind = lower_bound(endValLengthL.begin(),endValLengthL.begin()+k,nums[i]) - endValLengthL.begin(); //first index in array where the value is greater than this one, or we need to place a new one.
+    endValLengthL[ind] = nums[i];
+    endILengthL[ind] = i;
+    if (ind == k) {
+        k++;
+        endPoint = i;
+    }
+    if (ind == k-1) 
+    endPoint = i;
+    p[i] = (ind == 0) ? -1 : endILengthL[ind-1];      
+}
+// length = k
+printLis(endPoint);
+
 
 /* SYNTAX */
 
@@ -124,3 +149,75 @@ sort(liz.begin(), liz.end(), [v](point a, point b) {
 // Custom comparator lambda syntax
 auto comp = [polys] (int n) { return area(polys[n]) < area(polys[n]);};
 set<int, decltype(comp)> containingPolygons(comp);
+
+
+/*Network Flow*/
+// Push-Relabel algorithm
+int V;
+vector<vector<int>> capacity, flow;
+vector<int> height, excess;
+
+void push(int u, int v) {
+    int d = min(excess[u], capacity[u][v] - flow[u][v]);
+    flow[u][v] += d;
+    flow[v][u] -= d;
+    excess[u] -= d;
+    excess[v] += d;
+}
+
+void relabel(int u) {
+    int d = INF;
+    for (int i = 0; i < V; i++) {
+        if (capacity[u][i] - flow[u][i] > 0)
+            d = min(d, height[i]);
+    }
+    if (d < INF)
+        height[u] = d + 1;
+}
+
+vector<int> find_max_height_vertices(int s, int t) {
+    vector<int> max_height;
+    for (int i = 0; i < V; i++) {
+        if (i != s && i != t && excess[i] > 0) {
+            if (!max_height.empty() && height[i] > height[max_height[0]])
+                max_height.clear();
+            if (max_height.empty() || height[i] == height[max_height[0]])
+                max_height.push_back(i);
+        }
+    }
+    return max_height;
+}
+
+int max_flow(int s, int t) {
+    height.assign(V, 0);
+    height[s] = V;
+    flow.assign(V, vector<int>(V, 0));
+    excess.assign(V, 0);
+    excess[s] = INF;
+    for (int i = 0; i < V; i++) {
+        if (i != s)
+            push(s, i);
+    }
+
+    vector<int> current;
+    while (!(current = find_max_height_vertices(s, t)).empty()) {
+        for (int i : current) {
+            bool pushed = false;
+            for (int j = 0; j < V && excess[i]; j++) {
+                if (capacity[i][j] - flow[i][j] > 0 && height[i] == height[j] + 1) {
+                    push(i, j);
+                    pushed = true;
+                }
+            }
+            if (!pushed) {
+                relabel(i);
+                break;
+            }
+        }
+    }
+
+    int max_flow = 0;
+    for (int i = 0; i < V; i++)
+        max_flow += flow[i][t];
+    return max_flow;
+}
