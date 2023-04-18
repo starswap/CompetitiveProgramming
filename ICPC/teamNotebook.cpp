328918db74283f7adc131bc3079f6c6496307c6a
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-04-18 22:23:31 +0100

    Add KMP to notebook

--- ICPC/teamNotebook.cpp
+++ ICPC/teamNotebook.cpp
@@ -416,3 +416,33 @@ find_by_order(k) : K-th element in a set (counting from zero). //This is what yo
 // Fill a DP table
 fill(&dp[0][0][0][0], &dp[0][0][0][0] + sizeof(dp) / sizeof(dp[0][0][0][0]), -1);
 
+def KMP(STRING, PATTERN):
+
+    lps = [0 for i in range(len(PATTERN))]
+    i = 0
+    j = 1
+    while j < len(lps): # O(M)
+        if PATTERN[i] != PATTERN[j]:
+            j += 1
+            if i > 0:
+                i = lps[i-1]
+        else:
+            lps[j] = i+1
+            i += 1
+            j += 1
+
+    s = 0
+    p = 0
+    while s < len(STRING):
+        if STRING[s] != PATTERN[p]:
+            p = lps[p-1]
+            if p == 0:
+                s += 1    
+        else:
+            s += 1
+            p += 1
+        
+        if p == len(lps):
+            return True
+        
+    return False
