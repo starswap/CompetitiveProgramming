f6a610ff25f78505d795200afc803adfb8160e83
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-09-17 10:09:34 +0100

    Add comments

--- ICPC/UKIEPC/2021/K.cpp
+++ ICPC/UKIEPC/2021/K.cpp
@@ -17,9 +17,9 @@ int main() {
     bool across = false;
     while (true) {
         points.emplace_back(x ,y);
-        if (hypot(x - n, y - m) == l) {break;}
+        if (hypot(x - n, y - m) == l) {break;} // can go there directly for correct amount (prevents duplicate points)
         double nx = x, ny = y, nl = 0;
-        if (across) {
+        if (across) { // zig zagging
             nx = x + dx;
             nl = l - dx;
         } else {
@@ -27,15 +27,15 @@ int main() {
             nl = l - m;
         }
         across = !across;
-        if (hypot(nx - n, ny - m) > nl + EPS) {
-            if (y == 0) {
+        if (hypot(nx - n, ny - m) > nl + EPS) { // after the next zig zag there wouldn't be enough cable left.
+            if (y == 0) { // so calculate the amount we should go along the zigzag before going directly.
                 y = (- l * l + m * m + (n - x) * (n - x))/(2 * m - 2 * l);
             } else {
                 y = m - (((n - x) * (n - x) - l * l) / (-2 * l));
             }
             points.emplace_back(x ,y);
-            break;
-        } else {
+            break; // and we are done
+        } else { // otherwise continue zigzagging
             l = nl;
             x = nx;
             y = ny;
@@ -43,6 +43,7 @@ int main() {
     }
     points.emplace_back(n, m);
 
+    // Output
     cout << points.size() << endl;
     for (auto &[x, y] : points) {
         cout << x << " " << y << endl;
