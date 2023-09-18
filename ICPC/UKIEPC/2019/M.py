0013c70dc704df4feac7cd987a6d2ddb9ed5f468
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-09-18 12:06:07 +0100

    Make M.py more robust

--- ICPC/UKIEPC/2019/M.py
+++ ICPC/UKIEPC/2019/M.py
@@ -1,6 +1,33 @@
 import random
 n, m, c = map(int, input().split())
-colours = [random.randint(100000000, 1000000000) for i in range(c)]
+
+# check both Robin methods
+
+class Hash:
+    def __init__(self, hashes):
+        self.hashes = hashes
+    def __hash__(self) -> int:
+        return self.hashes.__hash__()
+    def __eq__(self, other):
+        return self.hashes == other.hashes
+    def __add__(self, other):
+        return Hash((self.hashes[0] + other.hashes[0], self.hashes[1] + other.hashes[1]))
+    def __sub__(self, other):
+        return Hash((self.hashes[0] - other.hashes[0], self.hashes[1] - other.hashes[1]))
+
+class Hasher:
+    def __init__(self):
+        self.colours1 = [random.randint(100000000, 1000000000) for i in range(c)]
+        self.colours2 = [random.randint(100000000, 1000000000) for i in range(c)]
+
+    def compute_with(self, row, colours):
+        counts = count(row)
+        zeroed = [*map(lambda x : x - counts[0], counts)]
+        hashed = sum(map(lambda x, y : x * y, zeroed, colours))
+        return hashed
+
+    def compute(self, row):
+        return Hash((self.compute_with(row, self.colours1), self.compute_with(row, self.colours2)))
 
 def LSOne(x):
     return x & -x
@@ -27,18 +54,16 @@ if n == 1:
     else:
         print(0)
 else:
+    hasher = Hasher()
     row_hashes = []
     for row in range(n):
-        nums = map(int, input().split())
-        counts = count(nums)
-        zeroed = [*map(lambda x : x - counts[0], counts)]
-        hashed = sum(map(lambda x, y : x * y, zeroed, colours))
-        row_hashes.append(hashed)
+        nums = [*map(int, input().split())]
+        row_hashes.append(hasher.compute(nums))
     
     f = n // 2
     top_half_lookup = {}
     mask = 0
-    hash = 0
+    hash = Hash((0, 0))
     cnt = 0
     for i in range(1, 1 << f):
         if hash in top_half_lookup:
@@ -59,7 +84,7 @@ else:
         top_half_lookup[hash] = cnt
     e = n - f
     mask = 0
-    target_hash = 0
+    target_hash = Hash((0, 0))
     cnt = 0
     best = 0
     for i in range(1, 1 << e):
