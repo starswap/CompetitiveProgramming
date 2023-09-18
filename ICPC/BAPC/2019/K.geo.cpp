9f9f387c06dad0a3134a69ef88a7993aa613eae2
Hamish Starling <hamishstarling@hotmail.co.uk>
2023-09-18 18:51:26 +0100

    Simplify geometric solution

--- ICPC/BAPC/2019/K.geo.cpp
+++ ICPC/BAPC/2019/K.geo.cpp
@@ -36,25 +36,13 @@ struct vec {
   }
 };
 
-typedef vector<point> polygon;
-
-double dot(vec a, vec b) { 
-    return (a.x*b.x + a.y*b.y);
-}
-
 double cross(vec a, vec b) {
     return a.x*b.y - a.y*b.x;
 }
 
-// returns true if point r is on the same line as the line pq
-bool collinear(point p, point q, point r) {
-  return fabs(cross(vec(p, q), vec(p, r))) < EPS;
-}
-
-// note: to accept collinear points, we have to change the `> 0' to > -EPS
-// returns true if point r is on the left side of line pq
+// returns true if point r is on the left side of line pq, accepting collinear
 bool ccw(point p, point q, point r) {
-  return cross(vec(p, q), vec(p, r)) > 0;
+  return cross(vec(p, q), vec(p, r)) > -EPS;
 }
 
 double dist(point p1, point p2) {                // Euclidean distance
@@ -63,32 +51,28 @@ double dist(point p1, point p2) {                // Euclidean distance
 
 int main() {
     int n;
+    cin >> n;
+
     double Px, Py;
-    cin >> n >> Px >> Py;
+    cin >> Px >> Py;
     point P(Px, Py);
 
-    polygon poly;
     double Gix, Giy;
     cin >> Gix >> Giy;
     point O(Gix, Giy);
 
+    vector<point> poly_rest; // except O
     for (int i = 0; i < n - 1; ++i) {
         cin >> Gix >> Giy;
-        poly.push_back(point(Gix, Giy));
+        poly_rest.push_back(point(Gix, Giy));
     }
-    poly.push_back(poly[0]);
+    poly_rest.push_back(poly_rest[0]);
 
     vector<double> f(n, 0.0);
     for (int k = 0; k < n - 1; ++k) {
-        point Q = poly[k];
-        point R = poly[k+1];
-        if (collinear(O, Q, P)) {
-            double a = dist(O, P);
-            double b = dist(P, Q);
-            f[0] = b / (a + b);
-            f[k + 1] = a / (a + b);
-            break;
-        } else if (!ccw(O, P, Q) && ccw(O, P, R)) {
+        point Q = poly_rest[k];
+        point R = poly_rest[k+1];
+        if (!ccw(O, P, Q) && ccw(O, P, R)) { // in the correct triangle
             vec v1(O, Q);
             vec v2(O, R);
             vec x(O, P);
@@ -96,12 +80,13 @@ int main() {
             double k1 = (v1 * v2.x - v2 * v1.x).y;
             double k2 = (v1 * v2.y - v2 * v1.y).x;
 
+            // Use v1 and v2 as a basis
             vec i_b(v2.y/k2, -v1.y/k2);
             vec j_b(v2.x/k1, -v1.x/k1);
             vec x_b(i_b * x.x + j_b * x.y);
 
-            double a = x_b.x;
-            double b = x_b.y;
+            double a = x_b.x; // multiplier of OR vector
+            double b = x_b.y; // multiplier of OQ vector
 
             f[k + 1] = a;
             f[k + 2] = b;
